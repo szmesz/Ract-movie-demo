ad269f6ddf58d113b0da2fac0a9c64aa6ef0b35c
Vajdat <32224054+szmesz@users.noreply.github.com>
2020-07-09 15:14:33 +0200

    minor changes

--- src/components/maincomponents/MainContent.js
+++ src/components/maincomponents/MainContent.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class MainContent extends Component {
     render() {
         return(
-    <div className="no-content">{this.props.text}</div>
+            <div className="no-content">{this.props.text}</div>
         );}
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import ErrorBoundary from './components/errorcomponents/ErrorBoundary';
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
 );
